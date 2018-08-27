b0098f717545f32701f2245d372476f9d37ca99f
VianeyHernandez <42428086+VianeyHernandez@users.noreply.github.com>
2018-08-26 22:08:27 -0500

    Rename Tarea3.cpp to Tarea 3.cpp

--- Tarea 3.cpp
+++ Tarea 3.cpp
@@ -10,7 +10,7 @@ int main() {
 	while (n <= 0) {
 		cout << "Los numeros naturales no tienen decimales, no son fraccionarios y\n";
 		cout << "se encuentran siempre a la derecha del cero en la recta real y son infinitos porque\n";
-		cout << "incluyen a todos los elementos de una sucesión, es decir, 1, 2, 3, 4, 5, 6, 7, etc\n";
+		cout << "incluyen a todos los elementos de una sucesiÃ³n, es decir, 1, 2, 3, 4, 5, 6, 7, etc\n";
 		cout << "Ingresa un numero correcto:\n";
 		cin >> n;
 	}
